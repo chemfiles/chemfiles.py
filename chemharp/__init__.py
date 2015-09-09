da9572d1a543f92d187a9cba6348a80c3f866769
Guillaume Fraux <guillaume.fraux@ens.fr>
2015-09-09 11:46:30 +0200

    Add version attribute

--- chemharp/__init__.py
+++ chemharp/__init__.py
@@ -1,6 +1,8 @@
 # -*- coding=utf-8 -*-
 from __future__ import absolute_import, print_function, unicode_literals
 
+__version__ = "0.3.1"
+
 from .errors import ChemharpException, ArgumentError
 from .atom import Atom
 from .topology import Topology
