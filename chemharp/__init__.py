2902d569f26d2dd5d9b44a3a4c04828e3762cc2c
Guillaume Fraux <guillaume.fraux@ens.fr>
2015-10-01 21:00:35 +0200

    Update version to 0.4.0.dev0

--- chemharp/__init__.py
+++ chemharp/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding=utf-8 -*-
 from __future__ import absolute_import, print_function, unicode_literals
 
-__version__ = "0.3.1"
+__version__ = "0.4.0.dev0"
 
 from .errors import ChemharpException, ArgumentError
 from .atom import Atom, AtomType
