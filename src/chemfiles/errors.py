bd64450dc9f7506eac539643a0a3689e4f37b62f
Guillaume Fraux <guillaume.fraux@chimie-paristech.fr>
2017-03-01 13:53:47 +0100

    Simplify the number of exceptions

--- src/chemfiles/errors.py
+++ src/chemfiles/errors.py
@@ -5,43 +5,33 @@ import chemfiles
 
 
 class ChemfilesWarning(UserWarning):
-    '''Warnigns coming from the Chemfiles C++ runtime'''
+    '''Warnings coming from the Chemfiles runtime.'''
     pass
 
 
 class ChemfilesException(BaseException):
-    '''Base class for all Chemfiles exceptions'''
-    pass
-
-
-class NullPointerError(ChemfilesException):
-    '''Got a NULL pointer from C!'''
-    def __init__(self):
-        message = "Got a NULL pointer from C! Today is a bad day."
-        super(NullPointerError, self).__init__(message)
-
-
-class CxxException(ChemfilesException):
-    '''Error in the C++ runtime'''
-    def __init__(self, status):
+    '''Error in the chemfiles runtime.'''
+    def __init__(self, status=None, message=""):
         self.status = status
-        super(CxxException, self).__init__(last_error())
+        if message == "":
+            message = last_error()
+        super(ChemfilesException, self).__init__(message)
 
 
 def last_error():
-    '''Get the last error from the library'''
+    '''Get the last error from the chemfiles runtime.'''
     return chemfiles.get_c_library().chfl_last_error().decode("utf8")
 
 
 def clear_errors():
-    '''Clear any error message saved in the library'''
+    '''Clear any error message saved in the chemfiles runtime.'''
     return chemfiles.get_c_library().chfl_clear_errors()
 
 
 def _check_return_code(status, _function, _arguments):
     '''Check that the function call was OK, and raise an exception if needed'''
     if status.value != 0:
-        raise CxxException(status)
+        raise ChemfilesException(status=status)
 
 
 def _check_handle(handle):
@@ -49,7 +39,9 @@ def _check_handle(handle):
     try:
         handle.contents
     except ValueError:
-        raise NullPointerError()
+        raise ChemfilesException(
+            message="Got a NULL pointer from the chemfiles runtime."
+        )
 
 
 # Store a reference to the last logging callback, to preven Python from
