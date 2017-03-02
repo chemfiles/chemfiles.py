57df76a153cb9a0bb7aacba8d9df7373f7907fe7
Guillaume Fraux <guillaume.fraux@chimie-paristech.fr>
2017-03-02 10:53:40 +0100

    Fix condition for documentation deployement

--- scripts/deploy-docs.sh
+++ scripts/deploy-docs.sh
@@ -1,16 +1,16 @@
 #!/bin/bash -xe
 
 # Exit early if we don't want to deploy the docs
-if [[ "$TRAVIS_BRANCH" != "master" || "$TRAVIS_TAG" == "" || "${TRAVIS_PULL_REQUEST}" == "true" ]]; then
+if [[ ("$TRAVIS_BRANCH" != "master" && "$TRAVIS_TAG" == "") || "${TRAVIS_PULL_REQUEST}" == "true" ]]; then
     exit 0
 fi
 
 # Install doc dependencies
-cd $TRAVIS_BUILD_DIR/build
 pip install sphinx
-pip install -r doc/requirements.txt
+pip install -r $TRAVIS_BUILD_DIR/doc/requirements.txt
 
 # Build documentation
+cd $TRAVIS_BUILD_DIR/build
 cmake -DCHFL_PY_BUILD_DOCUMENTATION=ON .
 make python_doc_html
 rm -rf doc/html/_static/bootswatch-* doc/html/_static/bootstrap-2.3.2
@@ -21,9 +21,9 @@ git checkout gh-pages
 # Copy the right directory
 if [[ "$TRAVIS_BRANCH == master" ]]; then
     rm -rf latest
-    mv ../doc/doc/html/ latest
+    mv ../build/doc/html/ latest
 elif [[ "$TRAVIS_TAG" != "" ]]; then
-    mv ../doc/doc/html/ $TRAVIS_TAG
+    mv ../build/doc/html/ $TRAVIS_TAG
 else
     echo "We should have exited earlier"
     exit 1
