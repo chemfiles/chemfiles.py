66672c2fb2d2531751e0bd2e46bbb89de203ecda
Guillaume Fraux <guillaume.fraux@chimie-paristech.fr>
2017-07-08 15:58:46 +0200

    Check if Python and chemfiles.dll are both 32 or 64-bit

--- src/chemfiles/clib.py
+++ src/chemfiles/clib.py
@@ -1,7 +1,9 @@
 # -* coding: utf-8 -*
 import os
 import sys
+import struct
 from ctypes import cdll, c_double, POINTER
+from ctypes import sizeof, c_voidp
 
 from .location import CHEMFILES_LOCATION, __file__ as location_file_path
 
@@ -12,8 +14,9 @@ class FindChemfilesLibrary(object):
 
     def __call__(self):
         if self._cache is None:
+            check_dll(CHEMFILES_LOCATION)
             try:
-                self._cache = cdll.LoadLibrary(lib_path(CHEMFILES_LOCATION))
+                self._cache = cdll.LoadLibrary(CHEMFILES_LOCATION)
             except OSError:
                 if location_file_path.endswith(".pyc"):
                     path = location_file_path[:-1]
@@ -36,19 +39,34 @@ class FindChemfilesLibrary(object):
         return self._cache
 
 
-def lib_path(location):
-    """Library file name from the location"""
-    if os.path.isfile(location):
-        return location
+def check_dll(path):
+    if not sys.platform.startswith("win"):
+        return
+    if not os.path.isfile(path):
+        return
 
-    if sys.platform.startswith("win"):
-        return location + ".dll"
-    elif sys.platform.startswith("linux"):
-        return "lib" + location + ".so"
-    elif sys.platform.startswith("darwin"):
-        return "lib" + location + ".dylib"
-    else:
-        raise OSError("Unknown os. Edit this file to add logic for your OS.")
+    import struct
+
+    IMAGE_FILE_MACHINE_I386 = 332
+    IMAGE_FILE_MACHINE_AMD64 = 34404
+
+    machine = None
+    with open(path, 'rb') as fd:
+        header = fd.read(2).decode(encoding="utf-8", errors="strict")
+        if header != "MZ":
+            raise ImportError(path + " is not a DLL")
+        else:
+            fd.seek(60)
+            header = fd.read(4)
+            header_offset = struct.unpack("<L", header)[0]
+            fd.seek(header_offset + 4)
+            header = fd.read(2)
+            machine = struct.unpack("<H", header)[0]
+
+    if sizeof(c_voidp) == 4 and machine != IMAGE_FILE_MACHINE_I386:
+        raise ImportError("Python is 32-bit, but chemfiles.dll is not")
+    elif sizeof(c_voidp) == 8 and machine != IMAGE_FILE_MACHINE_AMD64:
+        raise ImportError("Python is 64-bit, but chemfiles.dll is not")
 
 
 _get_c_library = FindChemfilesLibrary()
