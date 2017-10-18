b64b5f17c093d5850c7326d9b3455eb35096e632
Guillaume Fraux <guillaume.fraux@chimie-paristech.fr>
2017-10-18 15:03:57 +0000

    Add the Property class

--- src/chemfiles/property.py
+++ src/chemfiles/property.py
@@ -1,8 +1,82 @@
 # -*- coding=utf-8 -*-
 from __future__ import absolute_import, print_function, unicode_literals
+import sys
+from ctypes import c_double, c_bool
 
+from .ffi import chfl_property_kind, chfl_vector3d
 from .utils import CxxPointer, call_with_growing_buffer
+from .errors import ChemfilesError
 
 
 class Property(CxxPointer):
-    pass
+    '''
+    A :py:class:`Property` holds the data used in properties in
+    :py:class:`Frame` and :py:class:`Atom`. A property can have various types:
+    bool, double, string or 3D vectors.
+
+    This class is not meant for direct use, but is an internal class.
+    '''
+
+    def __init__(self, value):
+        '''Create a new property containing the given value'''
+        if isinstance(value, bool):
+            ptr = self.ffi.chfl_property_bool(c_bool(value))
+        elif isinstance(value, (float, int)):
+            ptr = self.ffi.chfl_property_double(c_double(value))
+        elif _is_string(value):
+            ptr = self.ffi.chfl_property_string(value.encode("utf8"))
+        elif _is_vector3d(value):
+            value = chfl_vector3d(value[0], value[1], value[2])
+            ptr = self.ffi.chfl_property_vector3d(value)
+        else:
+            raise ChemfilesError(
+                "can not create a Property with this value"
+            )
+
+        super(Property, self).__init__(ptr)
+
+    def __del__(self):
+        if hasattr(self, 'ptr'):
+            self.ffi.chfl_property_free(self)
+
+    def get(self):
+        kind = chfl_property_kind()
+        self.ffi.chfl_property_get_kind(self, kind)
+        if kind.value == chfl_property_kind.CHFL_PROPERTY_BOOL:
+            value = c_bool()
+            self.ffi.chfl_property_get_bool(self, value)
+            return value.value
+        if kind.value == chfl_property_kind.CHFL_PROPERTY_DOUBLE:
+            value = c_double()
+            self.ffi.chfl_property_get_double(self, value)
+            return value.value
+        if kind.value == chfl_property_kind.CHFL_PROPERTY_STRING:
+            def callback(buffer, size):
+                self.ffi.chfl_property_get_string(self, buffer, size)
+            return call_with_growing_buffer(callback, initial=32)
+        if kind.value == chfl_property_kind.CHFL_PROPERTY_VECTOR3D:
+            value = chfl_vector3d()
+            self.ffi.chfl_property_get_vector3d(self, value)
+            return value[0], value[1], value[2]
+        else:
+            raise ChemfilesError("unknown property kind, this is a bug")
+
+
+def _is_string(value):
+    if sys.version_info[0] == 3:
+        return isinstance(value, str)
+    else:
+        return isinstance(value, basestring)
+
+
+def _is_vector3d(value):
+    try:
+        if not isinstance(value[0], (float, int)):
+            return False
+        if not isinstance(value[1], (float, int)):
+            return False
+        if not isinstance(value[2], (float, int)):
+            return False
+        return True
+    except:
+        return False
---
+++ tests/property.py
@@ -0,0 +1,41 @@
+# -*- coding=utf-8 -*-
+from __future__ import absolute_import, print_function, unicode_literals
+import unittest
+import copy
+import numpy as np
+
+from chemfiles import Property, ChemfilesError
+
+
+class TestProperty(unittest.TestCase):
+    def test_bool(self):
+        prop = Property(False)
+        self.assertEqual(prop.get(), False)
+
+    def test_number(self):
+        prop = Property(22)
+        self.assertEqual(prop.get(), 22.0)
+
+        prop = Property(42.2)
+        self.assertEqual(prop.get(), 42.2)
+
+    def test_string(self):
+        prop = Property("foo")
+        self.assertEqual(prop.get(), "foo")
+
+    def test_vector3d(self):
+        prop = Property((3, 4, 5))
+        self.assertEqual(prop.get(), (3, 4, 5))
+
+        prop = Property([3, 4, 5])
+        self.assertEqual(prop.get(), (3, 4, 5))
+
+        prop = Property(np.array([3, 4, 5]))
+        self.assertEqual(prop.get(), (3, 4, 5))
+
+    def test_bad(self):
+        self.assertRaises(ChemfilesError, Property, ArithmeticError)
+
+
+if __name__ == '__main__':
+    unittest.main()
