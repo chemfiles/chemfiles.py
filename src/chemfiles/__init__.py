7982951356c3f99a45bc9f89304136b0507437b3
Guillaume Fraux <guillaume.fraux@chimie-paristech.fr>
2017-10-18 15:28:45 +0000

    Add Chemfiles.add_configuration

--- src/chemfiles/__init__.py
+++ src/chemfiles/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding=utf-8 -*-
 from __future__ import absolute_import, print_function, unicode_literals
 
-from .utils import ChemfilesError
+from .utils import ChemfilesError, set_warnings_callback, add_configuration
 from .atom import Atom
 from .residue import Residue
 from .topology import Topology
--- src/chemfiles/utils.py
+++ src/chemfiles/utils.py
@@ -72,6 +72,21 @@ def set_warnings_callback(function):
     _get_c_library().chfl_set_warning_callback(_CURRENT_CALLBACK)
 
 
+def add_configuration(path):
+    '''
+    Read configuration data from the file at ``path``.
+
+    By default, chemfiles reads configuration from any file name `.chemfilesrc`
+    in the current directory or any parent directory. This function can be used
+    to add data from another configuration file.
+
+    This function will fail if there is no file at ``path``, or if the file is
+    incorectly formatted. Data from the new configuration file will overwrite
+    any existing data.
+    '''
+    _get_c_library().chfl_add_configuration(path.encode("utf8"))
+
+
 def _last_error():
     '''Get the last error from the chemfiles runtime.'''
     return _get_c_library().chfl_last_error().decode("utf8")
