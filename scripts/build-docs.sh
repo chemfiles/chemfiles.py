da4f8baee820a8a8d80cd3ee107b9c88f72addf1
Guillaume Fraux <guillaume.fraux@chimie-paristech.fr>
2017-12-07 18:00:50 +0000

    Update CI builds

--- scripts/build-docs.sh
+++ scripts/build-docs.sh
@@ -1,25 +1,20 @@
 #!/bin/bash -xe
 
-# Install doc dependencies
-cd $TRAVIS_BUILD_DIR
-pip install -r doc/requirements.txt
-
 # Get previous documentation
 git clone https://github.com/$TRAVIS_REPO_SLUG --branch gh-pages gh-pages
 rm -rf gh-pages/.git
 
 # Build documentation
-cd $TRAVIS_BUILD_DIR/build
-cmake -DCHFL_PY_BUILD_DOCUMENTATION=ON .
-make python_doc_html
-rm -rf doc/html/_static/bootswatch-* doc/html/_static/bootstrap-2.3.2
+cd $TRAVIS_BUILD_DIR/doc
+make html
+rm -rf _build/html/_static/bootswatch-* _build/html/_static/bootstrap-2.3.2
 
 cd ../gh-pages
 
 # Copy the right directory
 if [[ "$TRAVIS_TAG" != "" ]]; then
-    mv ../build/doc/html/ $TRAVIS_TAG
+    mv ../doc/_build/html/ $TRAVIS_TAG
 else
     rm -rf latest
-    mv ../build/doc/html/ latest
+    mv ../doc/_build/html/ latest
 fi
