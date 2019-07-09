f5affddf3bd693af9e6ded39b2a107e1ccbaf196
Guillaume Fraux <guillaume.fraux@chimie-paristech.fr>
2019-07-09 23:38:34 +0200

    Clean more files

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -3,8 +3,12 @@
 set -ex
 
 find . -name "*.pyc" -delete
+find . -name "__pycache__" | xargs rm -rf
 
 rm -rf .tox
 rm -rf _skbuild
 rm -rf dist
 rm -rf chemfiles.egg-info
+rm -rf chemfiles/lib
+rm -rf chemfiles/include
+rm -rf chemfiles/external.py
