b2c947c806d461362d14affb57319fb9b130b931
Luthaf <luthaf@luthaf.fr>
2020-12-01 17:01:51 +0100

    Update the project to use pyproject.toml

--- setup.py
+++ setup.py
@@ -1,8 +1,16 @@
 # -*- coding=utf-8 -*-
-import chemfiles
-import sys
 from wheel.bdist_wheel import bdist_wheel
 from skbuild import setup
+
+import sys
+import os
+import re
+
+
+# Read the version from chemfiles/__init__.py without importing chemfiles
+__version__ = re.search(
+    r'__version__\s*=\s*[\'"]([^\'"]*)[\'"]', open("chemfiles/__init__.py").read()
+).group(1)
 
 
 class universal_wheel(bdist_wheel):
@@ -12,49 +20,48 @@ class universal_wheel(bdist_wheel):
         return ("py2.py3", "none") + tag[2:]
 
 
-with open("requirements.txt", "r") as fd:
-    requirements = list(filter(bool, (line.strip() for line in fd)))
-    if sys.hexversion >= 0x03040000:
-        requirements = list(filter(lambda r: r != "enum34", requirements))
+install_requires = ["numpy"]
+if sys.hexversion < 0x03040000:
+    install_requires.append("enum34")
 
 
-long_description = ""
-with open("README.md", "r") as fd:
-    for line in fd:
-        # remove github badges
-        if not line.startswith("[!["):
-            long_description += line
+# scikit-build options
+cmake_args = []
+if sys.platform.startswith("darwin"):
+    cmake_args.append("-DCMAKE_OSX_DEPLOYMENT_TARGET:STRING=10.9")
+
+if os.getenv("CHFL_PY_INTERNAL_CHEMFILES"):
+    cmake_args.append("-DCHFL_PY_INTERNAL_CHEMFILES=ON")
+
+
+def _get_lib_ext():
+    if sys.platform.startswith("win32"):
+        ext = ".dll"
+    elif sys.platform.startswith("darwin"):
+        ext = ".dylib"
+    elif sys.platform.startswith("linux"):
+        ext = ".so"
+    else:
+        raise Exception("Unknown operating system: %s" % sys.platform)
+    return ext
+
 
 setup(
-    name="chemfiles",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    version=chemfiles.__version__,
-    author="Guillaume Fraux",
-    author_email="luthaf@luthaf.fr",
-    description="Read and write chemistry trajectory files",
-    keywords="chemistry computational cheminformatics files formats",
-    url="http://github.com/chemfiles/chemfiles.py",
-    packages=["chemfiles"],
-    zip_safe=False,
-    install_requires=requirements,
-    setup_requires=["scikit-build"],
-    classifiers=[
-        "Development Status :: 4 - Beta",
-        "Environment :: Console",
-        "Intended Audience :: Developers",
-        "Intended Audience :: Science/Research",
-        "License :: OSI Approved :: BSD License",
-        "Operating System :: MacOS :: MacOS X",
-        "Operating System :: Microsoft :: Windows",
-        "Operating System :: Unix",
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 3",
-        "Topic :: Scientific/Engineering :: Chemistry",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-        "Topic :: Utilities",
-    ],
-    cmake_install_dir="chemfiles",
-    cmake_args=['-DCMAKE_OSX_DEPLOYMENT_TARGET:STRING=10.9'],
+    version=__version__,
+    install_requires=install_requires,
     cmdclass={"bdist_wheel": universal_wheel},
+    # cmake_install_dir="chemfiles",
+    cmake_args=cmake_args,
+    packages=["chemfiles"],
+    package_data={
+        "chemfiles": [
+            "*" + _get_lib_ext(),
+            "bin/*" + _get_lib_ext(),
+        ]
+    },
+    exclude_package_data={
+        "chemfiles": [
+            "include/*",
+        ]
+    },
 )
